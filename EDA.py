c0cf058026a73caf2605b353099e1ac2b372b926
xchen715 <wilsonhc715@gmail.com>
2020-09-20 17:47:11 -0400

    EDA: add two variables heapmap

--- EDA.py
+++ EDA.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from pyspark.sql import functions as F
 
+
 class EDA():
     def __init__(self, target_ctr):
         self.target_ctr = target_ctr
@@ -52,7 +53,7 @@ class EDA():
         low_ctr_ids = df[df[f"{id_col}_ctr"] < df[f"{id_col}_ctr"].quantile(0.75)].index
         return (list(high_ctr_ids), list(low_ctr_ids))
 
-    def cat_analysis(self,raw_df,col,prime_id):
+    def cat_analysis(self, raw_df, col, prime_id):
         """
         plotting out the CTR distribution of each category
 
@@ -70,23 +71,23 @@ class EDA():
         None
         """
 
-        dist = raw_df[prime_id,col].groupBy(col).agg(F.countDistinct(prime_id)).toPandas()
+        dist = raw_df[prime_id, col].groupBy(col).agg(F.countDistinct(prime_id)).toPandas()
         dist[col] = dist[col].astype(int)
         dist.set_index(col).sort_index().plot.bar()
         plt.show()
         count = raw_df.select(col).groupBy(col).count().toPandas()
         count[col] = count[col].astype(int)
         count = count.set_index(col)
-        click = raw_df.select(col,"label_int").groupBy(col).sum("label_int").toPandas()
+        click = raw_df.select(col, "label_int").groupBy(col).sum("label_int").toPandas()
         click[col] = click[col].astype(int)
         click = click.set_index(col)
         count.index = click.index.astype(int)
         print("CTR")
-        (click.iloc[:,0]/count.iloc[:,0]).sort_index().plot.bar()
-        plt.hlines(self.target_ctr, -1, 1000,linestyles='dashed',colors="r")
+        (click.iloc[:, 0] / count.iloc[:, 0]).sort_index().plot.bar()
+        plt.hlines(self.target_ctr, -1, 1000, linestyles='dashed', colors="r")
         plt.show()
 
-    def high_low_ctr_group_dist(self, raw_df, col, prime_id, high_ctr_ids,low_ctr_ids):
+    def high_low_ctr_group_dist(self, raw_df, col, prime_id, high_ctr_ids, low_ctr_ids):
         """
         plotting out the distribution of high low ctr group in specific column
 
@@ -106,7 +107,7 @@ class EDA():
         -------
         None
         """
-        df = raw_df.select(prime_id, col, "label_int").groupBy(prime_id, col)\
+        df = raw_df.select(prime_id, col, "label_int").groupBy(prime_id, col) \
             .agg({prime_id: "count", "label_int": "sum"}).toPandas()
         high_df = df.set_index(prime_id).loc[high_ctr_ids].groupby(col).sum()
         high_df["ctr_group"] = "high_ctr"
@@ -123,4 +124,27 @@ class EDA():
         print("High Low CTR Group Distribution")
         agg_df[col] = agg_df[col].astype('int32')
         sns.barplot(col, y="count_pct", hue="ctr_group", data=agg_df)
+        plt.show()
+
+    def click_not_click_distrubitions(self, raw_df, col):
+        print("distritubtions of click or not")
+        df = raw_df.select(col, "label").groupBy("label", col).count().toPandas()
+        df = pd.merge(df, df.groupby("label").sum(), on='label')
+        df['pct'] = df['count_x'] / df['count_y']
+        sns.barplot(x=col, y="pct", hue="label", hue_order=['1', '0'], data=df)
+        plt.show()
+
+    def two_variables_heapmap(self, raw_df, col1, col2):
+        print(f"{col1} and {col2} CTR heatmap")
+        df = raw_df.select(col1, col2, "label") \
+            .groupby(col1, col2, "label").count().toPandas()
+        total = df.groupby([col1, col2]).sum().reset_index()
+        df = pd.merge(df, total, on=[col1, col2])
+        df = df[df['label'] == "1"]
+        df['ctr'] = df["count_x"] / df["count_y"]
+
+        pivot_df = pd.pivot_table(df[[col1, col2, 'ctr']], values='ctr',
+                                  index=[col1],
+                                  columns=col2)
+        sns.heatmap(pivot_df, cmap="YlOrBr", annot=pivot_df.values)
         plt.show()
